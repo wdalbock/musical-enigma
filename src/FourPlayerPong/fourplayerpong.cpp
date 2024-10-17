90adbb702a8ff2cb2ba57068608e588746f4ec89
wdalbock <126948288+wdalbock@users.noreply.github.com>
2024-10-17 13:45:04 +0800

    Additional balances to quadpong.

--- src/FourPlayerPong/fourplayerpong.cpp
+++ src/FourPlayerPong/fourplayerpong.cpp
@@ -33,10 +33,10 @@ struct Ball {
 
 // Define paddles for 4 players (top, bottom, left, right)
 Paddle paddles[4] = {
-    {200, 10, 140, 10, 10},  // Top paddle (Player 1)
-    {200, 380, 140, 10, 10}, // Bottom paddle (Player 2)
-    {100, 200, 10, 140, 10},  // Left paddle (Player 3)
-    {470, 200, 10, 140, 10}, // Right paddle (Player 4)
+    {200, 0, 150, 10, 10},  // Top paddle (Player 1)
+    {200, 390, 150, 10, 10}, // Bottom paddle (Player 2)
+    {90, 200, 10, 150, 10},  // Left paddle (Player 3)
+    {480, 200, 10, 150, 10}, // Right paddle (Player 4)
 };
 
 int paddleDirection[4] = {1, 1, 1, 1}; // Used to determine which way the paddles are currently moving.
@@ -71,6 +71,7 @@ void randomiseBall() {
 
     int dx = (rand() % 5) - 2;
     int dy = (rand() % 5) - 2;
+
     if (dx == 0) {
         dx = 1;
     }
@@ -82,12 +83,27 @@ void randomiseBall() {
     ball.dy = dy;
 }
 
+void displayGameOver() {
+    vga.clear(vga.rgb(0, 0, 0));
+    gfx.setCursor(100, 150);
+    gfx.setTextSize(3);
+    gfx.setTextColor(TFT_WHITE);
+    gfx.print("GAME OVER");
+
+    gfx.setCursor(100, 200);
+    gfx.setTextSize(2);
+    gfx.printf("You made %d collisions.", collisions);
+    vga.show();
+}
+
 void playCollisionSound() {
   tone(43, NOTE_B0, 150);
   noTone(43);
 }
 
 int updateBall() {
+    static int increase = 0;
+
     // Clear previous ball position
     gfx.fillCircle(ball.x, ball.y, 5, TFT_BLACK);
 
@@ -96,32 +112,36 @@ int updateBall() {
     ball.y += ball.dy * ball.speed;
 
     // Check for collisions with the paddles
-    if (ball.y <= paddles[0].y + paddles[0].height && ball.x >= paddles[0].x && ball.x <= paddles[0].x + paddles[0].width) {
+    if (ball.y <= 15 && ball.x >= paddles[0].x && ball.x <= paddles[0].x + paddles[0].width) {
         ball.dy = -ball.dy; // Bounce off the top paddle (player 1) 
-        ball.dx = ball.dx + isMoving[0] * ((paddles[0].speed * paddleDirection[0])/10); // The speed of the ball is influenced by the current speed/direction of the paddle.
+        ball.dx = ball.dx + 2 * isMoving[0] * ((paddles[0].speed * paddleDirection[0])/10); // The speed of the ball is influenced by the current speed/direction of the paddle.
         // If isMoving[0] = 0, the ball speed remains the same. 
         collisions++;
+        increase = 1;
         playCollisionSound();
     }
 
-    if (ball.y >= paddles[1].y - 5 && ball.x >= paddles[1].x && ball.x <= paddles[1].x + paddles[1].width) {
+    if (ball.y >= 385 && ball.x >= paddles[1].x && ball.x <= paddles[1].x + paddles[1].width) {
         ball.dy = -ball.dy; 
-        ball.dx = ball.dx + isMoving[1] * ((paddles[1].speed * paddleDirection[1])/10);
+        ball.dx = ball.dx + 2 * isMoving[1] * ((paddles[1].speed * paddleDirection[1])/10);
         collisions++;
+        increase = 1;
         playCollisionSound();
     }
 
-    if (ball.x <= paddles[2].x + paddles[2].width && ball.y >= paddles[2].y && ball.y <= paddles[2].y + paddles[2].height) {
+    if (ball.x <= 105 && ball.y >= paddles[2].y && ball.y <= paddles[2].y + paddles[2].height) {
         ball.dx = -ball.dx; 
-        ball.dy = ball.dy + isMoving[2] * ((paddles[2].speed * paddleDirection[2])/10);
+        ball.dy = ball.dy + 2 * isMoving[2] * ((paddles[2].speed * paddleDirection[2])/10);
         collisions++;
+        increase = 1;
         playCollisionSound();
     }
 
-    if (ball.x >= paddles[3].x - 5 && ball.y >= paddles[3].y && ball.y <= paddles[3].y + paddles[3].height) {
+    if (ball.x >= 475 && ball.y >= paddles[3].y && ball.y <= paddles[3].y + paddles[3].height) {
         ball.dx = -ball.dx;
-        ball.dy = ball.dy + isMoving[3] * ((paddles[3].speed * paddleDirection[3])/10);
+        ball.dy = ball.dy + 2 * isMoving[3] * ((paddles[3].speed * paddleDirection[3])/10);
         collisions++;
+        increase = 1;
         playCollisionSound();
     }
 
@@ -131,6 +151,12 @@ int updateBall() {
     }
     if (ball.y <= 10 || ball.y >= 390) {
         return 1;
+    }
+
+    // Increase speed of ball every five collisions.
+    if (increase && collisions % 5 == 0) {
+        ball.speed++;
+        increase = 0;
     }
 
     drawBall();
@@ -220,8 +246,10 @@ int fourplayerpongmain() {
             break;
         }
     }
+    displayGameOver();
+    delay(3000);
 
-    ball = {0, 0, 0, 0, 2};
+    ball.speed = 2;
     int temp = collisions;
     collisions = 0;
     return temp;
